94b6d1fbfcb713a21f08826c19a8f5cdb1f80bf0
shaun <dukenan006@163.com>
2022-07-09 00:37:47 +0800

    fix(shell): update shell script.

--- update_db.sh
+++ update_db.sh
@@ -7,12 +7,6 @@ ts=`date +%Y%m%d`
 file="save_data.py"
 
 if [ $# -eq 1 ]; then
-    ps -ef | grep "$proj_dir/$file" | grep -v "grep" | awk '{print $2}' | while read pid; do kill -9 $pid; done
-    sleep 1
-fi
-
-cnt=$(ps -ef | grep  "proj_dir/$file" | grep -v "grep"| wc -l)
-
-if [ $cnt -eq 0 ];then
-    /home/shaun/.envs/spider_py3/bin/python3 $proj_dir/$file >> $proj_dir/logs/${file}.${ts}.log 2>&1 &
+  cd $proj_dir
+  /home/shaun/.envs/spider_py3/bin/python3 $file >> $proj_dir/logs/${file}.${ts}.log 2>&1 &
 fi
