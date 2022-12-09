a3580235d088ad9a8e397545e0d70dcd4f09f35c
shaun <dukenan006@163.com>
2022-12-09 17:09:36 +0800

    modify shell script

--- update_db.sh
+++ update_db.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-proj_dir=$(cd $(dirname $0); pwd)
+proj_dir=$(cd $(dirname $0) || exit; pwd)
 
-ts=`date +%Y%m%d`
+ts=$(date +%Y%m%d)
 
 file="save_data.py"
 
 if [ $# -eq 1 ]; then
-  cd $proj_dir
-  /home/shaun/.envs/spider_py3/bin/python3 $file >> $proj_dir/logs/${file}.${ts}.log 2>&1 &
+  cd "$proj_dir" || exit
+  /home/shaun/.envs/spider_py3/bin/python3 $file >> "$proj_dir"/logs/${file}."${ts}".log 2>&1 &
 fi
